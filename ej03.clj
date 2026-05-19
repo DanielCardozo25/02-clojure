0dc229c799c26bf43119b6fc1be9ffe3a6b50532
Charly <carlosecimino@gmail.com>
2026-05-19 09:18:16 -0300

    ej03 resuelto (3 versiones)

--- ej03.clj
+++ ej03.clj
@@ -1,3 +1,27 @@
-(defn entero? [x]  (and (number? x)       (== x (Math/floor x))))
-(defn div10[x](/ x 10))
-(defn sig-mul-10 [a] (if (= a 0) 1 (if (= (entero? (div10 a)) true) a (sig-mul-10 (+ a 1)) ) ) )
+(defn sig-mul-10-perez [a]
+  (int (* 
+            10 
+            (Math/floor 
+                (+ (/ a 10.0) 1)
+            )
+        )
+  )
+)
+
+(defn sig-mul-10-copes [n]
+    (let [q (quot n 10)
+          r (rem n 10)]
+    (* 10 (inc 
+            (if (and (neg? n) (not (zero? r))) ; ¿negativo no múltiplo de 10?
+                (dec q) 
+                 q
+            )
+        )
+    )
+    )
+)
+
+; Buscar la definición de 'mod' (no está en el apunte oficial)
+(defn sig-mul-10-baluja [a]
+    (+ a (- 10 (mod a 10)))
+)

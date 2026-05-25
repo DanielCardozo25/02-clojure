0dc22262192dc0128cca9d7daf372b6c32f882ac
DanielCardozo25 <daniel.cardozo@alu.inspt.utn.edu.ar>
2026-05-25 19:51:01 -0300

    Implement capicua function for palindrome check - funcional
    Define the function capicua to check if a number is a palindrome.

--- ej05.clj
+++ ej05.clj
@@ -1,3 +1,4 @@
 /*5. Definir la función capicua? que reciba un número entero no negativo de hasta 5
 dígitos y devuelva true si el número es capicúa; si no, false.*/
 
+(defn capicua [x] (if (and(> x 0) (< x 100000)) (let [original (str x) al-reves (apply str (reverse original))] (= original al-reves)) "Nro no valido"))

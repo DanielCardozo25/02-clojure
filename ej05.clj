9d4e0c75fa51940f5dfb8fa894968374252a1b96
DanielCardozo25 <daniel.cardozo@alu.inspt.utn.edu.ar>
2026-05-25 20:23:31 -0300

    Fix capicua function validation logic - funcional

--- ej05.clj
+++ ej05.clj
@@ -1,4 +1,4 @@
 /*5. Definir la función capicua? que reciba un número entero no negativo de hasta 5
 dígitos y devuelva true si el número es capicúa; si no, false.*/
 
-(defn capicua [x] (if (and(> x 0) (< x 100000)) (let [original (str x) al-reves (apply str (reverse original))] (= original al-reves)) "Nro no valido"))
+(defn capicua [x] (if (or (< x 0) (> x 99999)) "Nro invalido" (let[original (str x) al-reves (apply str (reverse original))] (= original al-reves) ) ))

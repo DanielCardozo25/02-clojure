6838bb699176f0f3381f1ca6d8f4e0df3359ddc0
DanielCardozo25 <daniel.cardozo@alu.inspt.utn.edu.ar>
2026-05-26 08:49:58 -0300

    listo

---
+++ ej08.clj
@@ -0,0 +1,5 @@
+/* Definir la función nth-fibo que reciba un número entero no negativo y devuelva el
+correspondiente término de la sucesión de Fibonacci. */
+
+
+(defn nth-fibo [x] (if (< x 0) "Nro no valido" (map #(* 1 (range 1 (+ x 1))) ) ) )

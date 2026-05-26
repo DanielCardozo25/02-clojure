9cb23683a7b1697f88fa53cf895df2b9f1ef1c5c
DanielCardozo25 <daniel.cardozo@alu.inspt.utn.edu.ar>
2026-05-26 09:01:51 -0300

    listo

---
+++ ej09.clj
@@ -0,0 +1,4 @@
+/*Definir la función cant-dig que reciba un número entero y devuelva la cantidad de
+dígitos que este tiene.*/
+
+(defn cant-dig [x] (println "Cantidad de digitos: " (count (str x)) ) )
